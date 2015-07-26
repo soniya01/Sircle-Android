15b4536c2931dc97c9e9870f1f738022ff49c06a
soniya01 <soniyapgadekar12@gmail.com>
2015-07-26 17:45:04 +0530

    Fixed newsletter title issue

--- app/src/main/java/com/app/sircle/UI/Fragment/NewsLetterFragment.java
+++ app/src/main/java/com/app/sircle/UI/Fragment/NewsLetterFragment.java
@@ -1,9 +1,9 @@
 package com.app.sircle.UI.Fragment;
 
 
+import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,16 +14,12 @@ import android.widget.Toast;
 import com.app.sircle.R;
 import com.app.sircle.UI.Activity.PDFViewer;
 import com.app.sircle.UI.Adapter.NewsLettersViewAdapter;
-import com.app.sircle.UI.Adapter.VideoListViewAdapter;
 import com.app.sircle.UI.Model.NewsLetter;
-import com.app.sircle.UI.Model.Video;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+
 public class NewsLetterFragment extends Fragment {
 
     private ListView newsLetterListView;
@@ -31,23 +27,17 @@ public class NewsLetterFragment extends Fragment {
     private List<NewsLetter> newsLetterList = new ArrayList<NewsLetter>();
 
 
-    public NewsLetterFragment() {
-        // Required empty public constructor
-    }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-      //  return inflater.inflate(R.layout.fragment_news_letter, container, false);
 
-        View viewFragment = inflater.inflate(R.layout.fragment_video,
+        View viewFragment = inflater.inflate(R.layout.fragment_news_letter,
                 null, true);
 
         populateDummyData();
 
-        newsLetterListView = (ListView)viewFragment.findViewById(R.id.fragment_video_list_view);
+        newsLetterListView = (ListView)viewFragment.findViewById(R.id.fragment_news_list_view);
         newsLetterListViewAdapter = new NewsLettersViewAdapter(getActivity(), newsLetterList);
 
         newsLetterListView.setAdapter(newsLetterListViewAdapter);
@@ -69,8 +59,6 @@ public class NewsLetterFragment extends Fragment {
 
     public void populateDummyData(){
         NewsLetter newsLetter = new NewsLetter();
-       // video.setVideoEmbedURL("https://www.youtube.com/watch?v=1uTVK2dXTUk");
-       // video.setVideoSource("Youtube");
         newsLetter.setPdfURL("http://fzs.sve-mo.ba/sites/default/files/dokumenti-vijesti/sample.pdf");
         newsLetter.setPdfTitle("PDF SAMPLE");
         newsLetter.setPdfDate("Wednesday 27 May 2015");
