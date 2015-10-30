409a5af15989a2a71dd68badc9bb286dd4a7e362
soniya01 <soniyapgadekar12@gmail.com>
2015-10-30 14:18:08 +0530

    Fixed push notification not getting fired issue

--- app/src/main/java/com/app/sircle/GCM/GCMListener.java
+++ app/src/main/java/com/app/sircle/GCM/GCMListener.java
@@ -1,25 +1,29 @@
 package com.app.sircle.GCM;
 
 import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
-import com.app.sircle.Manager.NotificationManager;
 import com.app.sircle.R;
 import com.app.sircle.UI.Activity.AddNotificationActivity;
+import com.app.sircle.UI.Activity.BaseActivity;
 import com.google.android.gms.gcm.GcmListenerService;
 
 /**
  * Created by soniya on 26/10/15.
  */
 public class GCMListener extends GcmListenerService {
+    private Notification notification;
 
     @Override
     public void onMessageReceived(String from, Bundle data) {
@@ -38,24 +42,50 @@ public class GCMListener extends GcmListenerService {
     }
 
     private void sendNotification(String message) {
-        Intent intent = new Intent(this, AddNotificationActivity.class);
+        Intent intent = new Intent(this, BaseActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
-                PendingIntent.FLAG_ONE_SHOT);
-
+                0);
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(android.R.drawable.stat_notify_voicemail)
-                .setContentTitle(getResources().getString(R.string.app_name))
-                .setContentText(message)
-                .setAutoCancel(true)
-                .setSound(defaultSoundUri)
-                .setContentIntent(pendingIntent);
 
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            notification = new Notification.Builder(this)
+                    .setContentTitle("Sircle")
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentIntent(pendingIntent)
+                    .setContentText(message)
+                    .setSound(defaultSoundUri)
+                    .setWhen(System.currentTimeMillis())
+                    .build();
+        }else {
+            notification = new NotificationCompat.Builder(this)
+                    .setSmallIcon(android.R.drawable.stat_notify_voicemail)
+                    .setContentTitle(getResources().getString(R.string.app_name))
+                    .setContentText(message)
+                    .setSound(defaultSoundUri)
+                    .setContentIntent(pendingIntent)
+            .build();
+        }
+        notification.flags = Notification.FLAG_AUTO_CANCEL | Notification.DEFAULT_LIGHTS;
 
-        notificationManager.notify();
+        NotificationManager mNotificationManager =
+                (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        mNotificationManager.notify(1, notification);
+
+
+//        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+//                .setSmallIcon(android.R.drawable.stat_notify_voicemail)
+//                .setContentTitle(getResources().getString(R.string.app_name))
+//                .setContentText(message)
+//                .setAutoCancel(true)
+//                .setSound(defaultSoundUri)
+//                .setContentIntent(pendingIntent);
+//
+//        android.app.NotificationManager notificationManager =
+//                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//
+//        notificationManager.notify();
 
         long futureInMillis = SystemClock.elapsedRealtime() + 1000;
         AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
