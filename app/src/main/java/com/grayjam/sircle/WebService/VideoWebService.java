413b859bdf6bd4a4199fd4d4c18b72ea5cf40e77
MOSES AFONSO <moses@stylabs.in>
2016-07-05 12:32:51 +0530

    Final Demo Version

---
+++ app/src/main/java/com/grayjam/sircle/WebService/VideoWebService.java
@@ -0,0 +1,43 @@
+package com.grayjam.sircle.WebService;
+
+import com.grayjam.sircle.Utility.AppError;
+import com.grayjam.sircle.Utility.Constants;
+import com.grayjam.sircle.WebService.Common.RetrofitImplementation;
+import com.grayjam.sircle.WebService.Common.WebServiceListener;
+
+import java.util.HashMap;
+
+/**
+ * Created by soniya on 25/08/15.
+ */
+public class VideoWebService {
+
+    private static VideoWebService sharedInstance;
+    private static RetrofitImplementation retrofitImplementation;
+
+    private VideoWebService(){
+
+    }
+
+    public static VideoWebService getSharedInstance(){
+        if (sharedInstance == null){
+            sharedInstance = new VideoWebService();
+            retrofitImplementation = new RetrofitImplementation();
+        }
+        return sharedInstance;
+    }
+
+    public void getAllVideos(HashMap object, final VideoWebServiceListener videoWebServiceListener){
+        retrofitImplementation.executePostWithURL(Constants.VIDEOS_GET_ALL_API, object, null, VideoResponse.class, new WebServiceListener() {
+            @Override
+            public void onCompletion(Object responseObject, AppError error) {
+                //List<Video> videoList = (ArrayList<Video>) responseObject;
+                videoWebServiceListener.onCompletion((VideoResponse)responseObject, error);
+            }
+        });
+    }
+
+    public interface VideoWebServiceListener{
+        public void onCompletion(VideoResponse response, AppError error);
+    }
+}
