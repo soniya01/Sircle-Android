05b969fb05d89f01dbd80732ff1cd26616b098d1
vikreddevil <vikreddevil@gmail.com>
2016-08-05 10:15:54 +0530

    nahar changes

---
+++ app/src/main/java/com/snaptech/naharInt/UI/Activity/SplashActivity.java
@@ -0,0 +1,203 @@
+package com.snaptech.naharInt.UI.Activity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.widget.Toast;
+
+import com.snaptech.naharInt.Manager.LoginManager;
+import com.snaptech.naharInt.Manager.LogoutManager;
+import com.snaptech.naharInt.R;
+import com.snaptech.naharInt.Utility.AppError;
+import com.snaptech.naharInt.Utility.Constants;
+import com.snaptech.naharInt.WebService.LogoutStatusResponse;
+
+import java.util.HashMap;
+
+
+public class SplashActivity extends Activity {
+
+    public static final int SPLASH_SCREEN_TIME_OUT = 3000;
+    private  Intent loginIntent = null;
+    private SharedPreferences loginSharedPreferences;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // check if user is already logged in
+//        if (checkIfLoggedIn()){
+//            intentClass = BaseActivity.class;
+//        }else {
+//            intentClass =LoginScreen.class;
+//        }
+
+        checkIfLoggedIn();
+
+        // add delay to the splash screen
+
+    }
+
+
+    /**
+     * Checks if user is already logged in to the app
+     * @return if user is logged in then success else fail
+     */
+    private void checkIfLoggedIn(){
+        loginSharedPreferences = this.getSharedPreferences(Constants.LOGIN_PREFS_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = loginSharedPreferences.edit();
+        String accessToken = loginSharedPreferences.getString(Constants.LOGIN_ACCESS_TOKEN_PREFS_KEY,null);
+
+        if (accessToken != null){
+            loginIntent = new Intent(SplashActivity.this, BaseActivity.class);
+            startActivity(loginIntent);
+            finish();
+
+//             checkUserStatus();
+
+            //}
+        }else {
+
+
+            loginIntent = new Intent(this, LoginScreen.class);
+
+
+
+
+        }
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                startActivity(loginIntent);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
+
+
+
+
+
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+
+        return super.dispatchTouchEvent(ev);
+    }
+
+    public void checkUserStatus()
+    {
+       final HashMap object = new HashMap();
+        //object.put("regId", Constants.GCM_REG_ID);
+        //object.put("groupId", grpIdString);
+        object.put("page", "1");
+
+
+        LoginManager.getSharedInstance().checkUserLogoutStatus(object, new LoginManager.LogoutStatusManagerListener() {
+            @Override
+            public void onCompletion(LogoutStatusResponse response, AppError error) {
+
+                if (error.getErrorCode() == 0) {
+                    // give access to the app features
+                    if (response != null){
+                        if (response.getStatus() == 200)
+                        {
+
+
+                            if (response.getData().getLogout() ==1)
+                            {
+
+                                LoginManager.getSharedInstance().checkUserLogoutStatus(object, new LoginManager.LogoutStatusManagerListener() {
+                                    @Override
+                                    public void onCompletion(LogoutStatusResponse response, AppError error) {
+                                    }
+                                        });
+
+                                LogoutManager.getSharedInstance().handleUserLogoutPreferences();
+
+                                loginIntent = new Intent(SplashActivity.this, LoginScreen.class);
+
+
+
+                                new Handler().postDelayed(new Runnable() {
+
+                                    @Override
+                                    public void run() {
+                                        startActivity(loginIntent);
+                                        finish();
+                                    }
+                                }, SPLASH_SCREEN_TIME_OUT);
+
+
+                            }
+                            else
+                            {
+                                loginIntent = new Intent(SplashActivity.this, BaseActivity.class);
+                                startActivity(loginIntent);
+                                finish();
+                            }
+
+                            // ringProgressDialog.dismiss();
+                            // Toast.makeText(ForgotPasswordActivity.this,"Please check your email for a reset link", Toast.LENGTH_SHORT).show();
+                            // usernameField.setText("");
+
+                        }
+                        else if (response.getStatus()==404)
+                        {
+                            loginIntent = new Intent(SplashActivity.this, BaseActivity.class);
+                            startActivity(loginIntent);
+                            finish();
+                        }
+
+                        else {
+
+
+//                            {
+//                                "code": 404,
+//                                    "message": "invalid request!",
+//                                    "data": {},
+//                                "error": {
+//                                "warning": "error_exists"
+//                            }
+//                            }
+//
+
+
+                         //   finish();
+                            //  ringProgressDialog.dismiss();
+                            // usernameField.setText("");
+
+                            Toast.makeText(SplashActivity.this,"Something went wrong please try again", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }else {
+
+                      //  finish();
+                        //ringProgressDialog.dismiss();
+                        // usernameField.setText("");
+
+                        // Toast.makeText(ForgotPasswordActivity.this, "Something went wrong please try again", Toast.LENGTH_SHORT).show();
+                    }
+                }else {
+
+                  //  finish();
+
+                    //ringProgressDialog.dismiss();
+                    //usernameField.setText("");
+
+                    //Toast.makeText(ForgotPasswordActivity.this, "Check internet connectivity", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+    }
+
+}
---
+++ app/src/main/java/com/snaptech/naharInt/UI/Adapter/CalendarDayListAdapter.java
@@ -0,0 +1,138 @@
+package com.snaptech.naharInt.UI.Adapter;
+
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.snaptech.naharInt.R;
+import com.snaptech.naharInt.UI.Model.Event;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by mosesafonso on 15/04/16.
+ */
+public class CalendarDayListAdapter extends BaseAdapter {
+    private Context context;
+    private List<Event> calendarMonthList = new ArrayList<Event>();
+    private LayoutInflater inflater;
+
+    public CalendarDayListAdapter(Context context, List<Event> calendarMonthList) {
+        this.context = context;
+        this.calendarMonthList = calendarMonthList;
+        inflater  = (LayoutInflater) this.context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return calendarMonthList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return calendarMonthList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            viewHolder = new ViewHolder();
+            convertView = inflater.inflate(R.layout.list_view_calendar_day,
+                    parent, false);
+
+            viewHolder.eventTitleLabel = (TextView) convertView.findViewById(R.id.eventTitle);
+            viewHolder.eventDateLabel = (TextView) convertView.findViewById(R.id.startTime);
+            viewHolder.eventTimeLabel = (TextView) convertView.findViewById(R.id.endTime);
+            viewHolder.eventImage = (ImageView) convertView.findViewById(R.id.eventImage);
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        viewHolder.eventTitleLabel.setText(calendarMonthList.get(position).getTitle());
+        viewHolder.eventDateLabel.setText(calendarMonthList.get(position).getStartTime());
+        viewHolder.eventTimeLabel.setText(calendarMonthList.get(position).getEndTime());
+
+        if (calendarMonthList.get(position).getCategoryName().equals("Arts"))
+        {
+            // .setBackgroundResource(R.drawable.arts);
+            viewHolder.eventImage.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.arts));
+        }
+        else if (calendarMonthList.get(position).getCategoryName().equals("Sports"))
+        {
+            // viewHolder.eventImage.setBackgroundResource(R.drawable.sports);
+            viewHolder.eventImage.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.sports));
+        }
+        else if (calendarMonthList.get(position).getCategoryName().equals(""))
+        {
+            // viewHolder.eventImage.setBackgroundResource(R.drawable.event_image);
+            viewHolder.eventImage.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.documentslist));
+        }
+        else if (calendarMonthList.get(position).getCategoryName().equals("Academics"))
+        {
+            viewHolder.eventImage.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.academics));
+        }
+        else if (calendarMonthList.get(position).getCategoryName().equals("Performance"))
+        {
+            viewHolder.eventImage.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.performance));
+        }
+        else if (calendarMonthList.get(position).getCategoryName().equals("Others"))
+        {
+            viewHolder.eventImage.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.others));
+        }
+
+
+//        if (calendarMonthList.get(position).getIconId()==1)
+//        {
+//            // .setBackgroundResource(R.drawable.arts);
+//            viewHolder.eventImage.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.arts));
+//        }
+//        else if (calendarMonthList.get(position).getIconId()==2)
+//        {
+//            // viewHolder.eventImage.setBackgroundResource(R.drawable.sports);
+//            viewHolder.eventImage.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.sports));
+//        }
+//        else if (calendarMonthList.get(position).getIconId()==3)
+//        {
+//            // viewHolder.eventImage.setBackgroundResource(R.drawable.event_image);
+//            viewHolder.eventImage.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.event_image));
+//        }
+//        else if (calendarMonthList.get(position).getIconId()==4)
+//        {
+//            viewHolder.eventImage.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.academics));
+//        }
+//        else if (calendarMonthList.get(position).getIconId()==5)
+//        {
+//            viewHolder.eventImage.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.performance));
+//        }
+//        else if (calendarMonthList.get(position).getIconId()==6)
+//        {
+//            viewHolder.eventImage.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.others));
+//        }
+
+
+        return convertView;
+    }
+
+    static class ViewHolder {
+        private TextView eventTitleLabel;
+        private TextView eventDateLabel;
+        private TextView eventTimeLabel;
+        private ImageView eventImage;
+    }
+}
