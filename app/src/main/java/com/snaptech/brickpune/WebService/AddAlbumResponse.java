c86b6e8f824c461b89d95219cab16fccfb5d865a
vikreddevil <vikreddevil@gmail.com>
2016-08-12 17:20:09 +0530

    new instance changes

---
+++ app/src/main/java/com/snaptech/brickpune/WebService/AddAlbumResponse.java
@@ -0,0 +1,40 @@
+package com.snaptech.brickpune.WebService;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by soniya on 23/09/15.
+ */
+public class AddAlbumResponse {
+
+    public String message;
+
+    @SerializedName("code")
+    public int status;
+
+    public AddAlbumResponseData data;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public AddAlbumResponseData getData() {
+        return data;
+    }
+
+    public void setData(AddAlbumResponseData data) {
+        this.data = data;
+    }
+}
