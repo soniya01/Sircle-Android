746d4b79fb03514039939f3b17363b3b1529f1c4
Vikas <vikas@Vikass-MacBook-Pro.local>
2016-09-07 17:59:48 +0530

    demo app changes

---
+++ app/src/main/java/com/snaptech/spanishdemo/WebService/NotificationResponseData.java
@@ -0,0 +1,67 @@
+package com.snaptech.spanishdemo.WebService;
+
+import com.snaptech.spanishdemo.UI.Model.Notification;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by soniya on 20/09/15.
+ */
+public class NotificationResponseData {
+
+//    @SerializedName("total_records")
+//    public int totalRecords;
+//    public int page;
+//
+//    @SerializedName("page_records")
+//    public int pageRecords;
+
+
+
+    List<Notification> notifications = new ArrayList<Notification>();
+
+    @SerializedName("method_type")
+    public String methodType;
+
+//    public int getTotalRecords() {
+//        return totalRecords;
+//    }
+//
+//    public void setTotalRecords(int totalRecords) {
+//        this.totalRecords = totalRecords;
+//    }
+//
+//    public int getPage() {
+//        return page;
+//    }
+//
+//    public void setPage(int page) {
+//        this.page = page;
+//    }
+//
+//    public int getPageRecords() {
+//        return pageRecords;
+//    }
+//
+//    public void setPageRecords(int pageRecords) {
+//        this.pageRecords = pageRecords;
+//    }
+
+    public List<Notification> getNotifications() {
+        return notifications;
+    }
+
+    public void setNotifications(List<Notification> notifications) {
+        this.notifications = notifications;
+    }
+
+    public String getMethodType() {
+        return methodType;
+    }
+
+    public void setMethodType(String methodType) {
+        this.methodType = methodType;
+    }
+}
---
+++ app/src/main/java/com/snaptech/spanishdemo/WebService/UserData.java
@@ -0,0 +1,88 @@
+package com.snaptech.spanishdemo.WebService;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by soniya on 02/09/15.
+ */
+public class UserData {
+
+    @SerializedName("user_id")
+    public String userId;
+
+    @SerializedName("user_type_id")
+    public String userTypeId;
+
+    @SerializedName("user_type")
+    public String userType;
+
+    public String email;
+
+    public AuthenticationData oauth;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserTypeId() {
+        return userTypeId;
+    }
+
+    public void setUserTypeId(String userTypeId) {
+        this.userTypeId = userTypeId;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public AuthenticationData getOauth() {
+        return oauth;
+    }
+
+    public void setOauth(AuthenticationData oauth) {
+        this.oauth = oauth;
+    }
+
+//       @SerializedName("customer_details")
+//        public String customerDetails;
+//
+//    @SerializedName("auth_token")
+//    public String authToken;
+//
+//
+//        public String getCustomerDetails() {
+//        return customerDetails;
+//    }
+//
+//    public void setCustomerDetails(String customerDetails) {
+//        this.customerDetails = customerDetails;
+//    }
+//
+//
+//    public String getAuthToken() {
+//        return authToken;
+//    }
+//
+//    public void setAuthToken(String authToken) {
+//        this.authToken = authToken;
+//    }
+
+
+}
