e4b0ff8e112db5d3972414291cf4c9686e87ce81
Vikas <vikreddevil@gmail.com>
2016-09-22 12:29:40 +0530

    syso comment

--- app/src/main/java/com/snaptech/kipling/UI/Activity/BaseActivity.java
+++ app/src/main/java/com/snaptech/kipling/UI/Activity/BaseActivity.java
@@ -125,7 +125,7 @@ public class BaseActivity extends AppCompatActivity implements CalendarMonthFrag
                         Constants.flag_logout=false;
                     }
                     else {
-                        System.out.println("Data received" + data.getMessage() + "version is" + data.getForcedUpdateData().android_version + " actual version code is " + versionCode);
+                        //System.out.println("Data received" + data.getMessage() + "version is" + data.getForcedUpdateData().android_version + " actual version code is " + versionCode);
                         versionCode = BuildConfig.VERSION_CODE;
                         checkForcedUpdate(data, error, versionCode);
                     }
