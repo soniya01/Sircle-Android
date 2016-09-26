d5bf668c60a20521e22d2d6e593e716926223658
Vikas <vikreddevil@gmail.com>
2016-09-26 17:02:47 +0530

    multiple notification issue

---
+++ app/src/main/java/com/snaptech/asianintl/WebService/CategoryResponseData.java
@@ -0,0 +1,22 @@
+package com.snaptech.asianintl.WebService;
+
+import com.snaptech.asianintl.UI.Model.EventCategory;
+
+import java.util.List;
+
+/**
+ * Created by mosesafonso on 30/05/16.
+ */
+public class CategoryResponseData {
+
+    public List<EventCategory> categories;
+
+    public List<EventCategory> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<EventCategory> categories) {
+        this.categories = categories;
+    }
+
+}
---
+++ app/src/main/java/com/snaptech/asianintl/WebService/Notificationservice.java
@@ -0,0 +1,111 @@
+package com.snaptech.asianintl.WebService;
+
+import com.snaptech.asianintl.Utility.AppError;
+import com.snaptech.asianintl.Utility.Constants;
+import com.snaptech.asianintl.WebService.Common.RetrofitImplementation;
+import com.snaptech.asianintl.WebService.Common.WebServiceListener;
+
+import java.util.HashMap;
+
+/**
+ * Created by soniya on 20/08/15.
+ */
+public class Notificationservice {
+    //public static List<NotificationGroups> notificationGroupsList = new ArrayList<NotificationGroups>();
+    private static Notificationservice sharedInstance;
+    private static RetrofitImplementation retrofitImplementation;
+
+    private Notificationservice(){
+
+    }
+
+    public static Notificationservice getSharedInstance(){
+        if (sharedInstance == null){
+            sharedInstance = new Notificationservice();
+            retrofitImplementation = new RetrofitImplementation();
+        }
+        return sharedInstance;
+    }
+
+    public void getAllGroups(HashMap map,final GroupsServiceListener groupsServiceListener){
+
+        retrofitImplementation.executeGetWithURL(Constants.NOTIFICATION_GET_ALL_GROUPS, map, null, GroupResponse.class, new WebServiceListener() {
+            @Override
+            public void onCompletion(Object response, AppError error) {
+                if (response != null){
+                    // notificationGroupsList = (ArrayList<NotificationGroups>) ((GroupResponse)response).getMessage();
+                    if (error.getErrorCode() == AppError.NO_ERROR || error == null){
+                        groupsServiceListener.onCompletion((GroupResponse)response, new AppError());
+                    }
+                    groupsServiceListener.onCompletion((GroupResponse)response, error);
+                }else {
+                    groupsServiceListener.onCompletion(null, error);
+                }
+            }
+        });
+
+    }
+
+    public void getAllNotifications(HashMap object, final NotificationServiceListener notificationServiceListener){
+        retrofitImplementation.executePostWithURL(Constants.NOTIFICATION_GET_API, object, null, NotificationResponse.class, new WebServiceListener() {
+            @Override
+            public void onCompletion(Object response, AppError error) {
+                notificationServiceListener.onCompletion((NotificationResponse) response, error);
+            }
+        });
+    }
+
+    public void getNotificationCount(HashMap object, final  NotificationCountServiceListener notificationCountServiceListener){
+        retrofitImplementation.executePostWithURL(Constants.NOTIFICATION_COUNT, object, null, NotificationCountResponse.class, new WebServiceListener() {
+            @Override
+            public void onCompletion(Object response, AppError error) {
+                if (response != null) {
+                    // notificationGroupsList = (ArrayList<NotificationGroups>) ((GroupResponse)response).getMessage();
+                    notificationCountServiceListener.onCompletion((NotificationCountResponse) response, new AppError());
+                }
+            }
+        });
+    }
+
+    public void updateGroupNot(HashMap object, final  PostServiceListener postServiceListener){
+        retrofitImplementation.executePostWithURL(Constants.GROUP_UPDATE_NOTIFICATION, object, null, PostResponse.class, new WebServiceListener() {
+            @Override
+            public void onCompletion(Object response, AppError error) {
+                if (response != null) {
+                    if (error.getErrorCode() == AppError.NO_ERROR || error == null) {
+                        postServiceListener.onCompletion((PostResponse) response, new AppError());
+                    }
+                } else {
+                    postServiceListener.onCompletion(null, error);}
+            }
+        });
+    }
+
+
+    public void addNotification(HashMap params, final PostServiceListener postServiceListener){
+        retrofitImplementation.executePostWithURL(Constants.NOTIFICATION_ADD_GROUPS, params, params, PostResponse.class, new WebServiceListener() {
+            @Override
+            public void onCompletion(Object responseObject, AppError error) {
+               postServiceListener.onCompletion((PostResponse)responseObject, error);
+            }
+        });
+    }
+
+    public interface NotificationServiceListener{
+        public void onCompletion(NotificationResponse response, AppError error);
+    }
+
+    public interface NotificationCountServiceListener{
+        public void onCompletion(NotificationCountResponse response, AppError error);
+    }
+
+    public interface GroupsServiceListener{
+        public void onCompletion(GroupResponse groupResponse, AppError error);
+    }
+
+    public interface PostServiceListener{
+        public void onCompletion(PostResponse postResponse, AppError error);
+    }
+
+
+}
