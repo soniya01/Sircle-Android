fdf7c614e08b8d980f838ef0170ec20ebe3c4d14
Vikas <vikreddevil@gmail.com>
2016-09-28 12:22:28 +0530

    English to Spanish

---
+++ app/src/main/java/com/snaptech/montesorriamerican/UI/Activity/AlbumFullScreenActivity.java
@@ -0,0 +1,208 @@
+package com.snaptech.montesorriamerican.UI.Activity;
+
+import android.app.ProgressDialog;
+import android.content.ContentValues;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.snaptech.montesorriamerican.Manager.PhotoManager;
+import com.snaptech.montesorriamerican.R;
+import com.snaptech.montesorriamerican.UI.Adapter.AlbumImagePagerAdapter;
+import com.snaptech.montesorriamerican.UI.Fragment.PhotosFragment;
+import com.snaptech.montesorriamerican.UI.Model.AlbumDetails;
+import com.snaptech.montesorriamerican.Utility.Constants;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class AlbumFullScreenActivity extends ActionBarActivity {
+
+    private ViewPager imageViewPager;
+    private AlbumImagePagerAdapter albumImagePagerAdapter;
+    private List<AlbumDetails> albumDetailsList = new ArrayList<AlbumDetails>();
+    ProgressDialog mProgressDialog;
+    int position;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        albumDetailsList.addAll(PhotoManager.getSharedInstance().albumDetailsList);
+
+        populateDummyData();
+        setContentView(R.layout.activity_album_full_screen);
+        imageViewPager = (ViewPager)findViewById(R.id.pager);
+
+        position = getIntent().getIntExtra("position",0);
+        getSupportActionBar().setTitle("");
+
+
+        albumImagePagerAdapter = new AlbumImagePagerAdapter(this, albumDetailsList);
+        imageViewPager.setAdapter(albumImagePagerAdapter);
+        imageViewPager.setCurrentItem(position);
+    }
+
+    void populateDummyData(){
+
+
+    }
+
+//    @Override
+//    public boolean onPrepareOptionsMenu(Menu menu) {
+//        // If the nav drawer is open, hide action items related to the content view
+//
+//
+//
+//        // boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+//        menu.findItem(R.id.action_settings).setVisible(false);
+//
+//        return super.onPrepareOptionsMenu(menu);
+//    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_album_full_screen, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        // Activate the navigation drawer toggle
+        if (item.getItemId() == R.id.action_download) {
+            //TODO: download
+            int position = imageViewPager.getCurrentItem();
+            String imageUrl = albumDetailsList.get(position).getFilePath();
+            new ImageDownloader().execute(imageUrl);
+            return true;
+        }if (item.getItemId() == android.R.id.home){
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+
+    public class ImageDownloader extends AsyncTask<String, Void, Bitmap>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            mProgressDialog = new ProgressDialog(AlbumFullScreenActivity.this);
+            // Set progress dialog title
+            //mProgressDialog.setTitle("Download Image Tutorial");
+            // Set progress dialog message
+            mProgressDialog.setMessage("Downloading...");
+            mProgressDialog.setIndeterminate(false);
+            // Show progress dialog
+            mProgressDialog.show();
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            String imageURL = params[0];
+
+            Bitmap bitmap = null;
+            try {
+                // Download Image from URL
+                InputStream input = new java.net.URL(imageURL).openStream();
+                // Decode Bitmap
+                bitmap = BitmapFactory.decodeStream(input);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            mProgressDialog.dismiss();
+            Toast.makeText(AlbumFullScreenActivity.this,"Imagen descargado con éxito",Toast.LENGTH_LONG).show();
+
+            // save image to gallery
+            storeImage(bitmap);
+        }
+
+
+        public void storeImage(Bitmap bitmap){
+                //get path to external storage (SD card)
+                String iconsStoragePath = Constants.PHOTO_SAVE_GALLERY_DIR_IMAGE_PATH  + PhotosFragment.albumName;
+                File sdIconStorageDir = new File(iconsStoragePath);
+
+                //create storage directories, if they don't exist
+            if (!sdIconStorageDir.mkdir()){
+                sdIconStorageDir.mkdirs();
+            }
+
+                try {
+                    String filePath = sdIconStorageDir.toString() + "/image-" + position +".jpeg";
+                    FileOutputStream fileOutputStream = new FileOutputStream(filePath);
+
+                    BufferedOutputStream bos = new BufferedOutputStream(fileOutputStream);
+
+                    //choose another format if PNG doesn't suit you
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bos);
+
+                    addImageToGallery(filePath);
+
+                    bos.flush();
+                    bos.close();
+
+                } catch (FileNotFoundException e) {
+                    Log.w("TAG", "Error saving image file: " + e.getMessage());
+
+                } catch (IOException e) {
+                    Log.w("TAG", "Error saving image file: " + e.getMessage());
+
+                }
+        }
+
+
+        public  void addImageToGallery(final String filePath) {
+
+            ContentValues values = new ContentValues();
+
+            values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
+            values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+            values.put(MediaStore.MediaColumns.DATA, filePath);
+
+            getApplicationContext().getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        }
+    }
+}
---
+++ app/src/main/java/com/snaptech/montesorriamerican/UI/Adapter/PhotosListViewAdapter.java
@@ -0,0 +1,96 @@
+package com.snaptech.montesorriamerican.UI.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.snaptech.montesorriamerican.R;
+import com.snaptech.montesorriamerican.UI.Model.Photo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by soniya on 23/07/15.
+ */
+public class PhotosListViewAdapter extends BaseAdapter {
+    private Context mContext;
+    private List<Photo> photoList = new ArrayList<Photo>();
+    private LayoutInflater inflater;
+
+    public PhotosListViewAdapter(Context mContext, List<Photo> photoList) {
+        this.mContext = mContext;
+        this.photoList = photoList;
+        inflater = (LayoutInflater) this.mContext
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return photoList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return photoList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return photoList.get(position).albumID;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            viewHolder = new ViewHolder();
+            convertView = inflater.inflate(R.layout.list_view_photos_row,
+                    parent, false);
+           // viewHolder.albumImageView = (ImageView) convertView.findViewById(R.id.photos_row_album_image);
+            viewHolder.albumLabelName = (TextView) convertView.findViewById(R.id.notification_row_title_label_name);
+            viewHolder.imagesCountLabel = (TextView) convertView.findViewById(R.id.notification_row_desc_label);
+            viewHolder.albumDate = (TextView) convertView.findViewById(R.id.links_row_publish_label_day);
+            viewHolder.albumTime = (TextView) convertView.findViewById(R.id.links_row_publish_label_time);
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+//        Picasso.with(mContext)
+//                .load(photoList.get(position).albumCoverImageURL)
+//                .into(viewHolder.albumImageView, new Callback() {
+//                    @Override
+//                    public void onSuccess() {
+//
+//                    }
+//
+//                    @Override
+//                    public void onError() {
+//
+//                    }
+//                });
+
+        viewHolder.imagesCountLabel.setText(String.valueOf(photoList.get(position).numberOfPhotos)+" Photos");
+        viewHolder.albumLabelName.setText(photoList.get(position).albumTitle);
+
+        String str = photoList.get(position).getPublishDate();
+        String[] splited = str.split("\\s+");
+
+        viewHolder.albumDate.setText(splited[0]);
+        viewHolder.albumTime.setText(splited[1]);
+
+        return convertView;
+    }
+
+    static class ViewHolder {
+
+      //  private ImageView albumImageView;
+        private TextView albumLabelName, imagesCountLabel;
+        private TextView albumDate, albumTime;
+    }
+}
