85edef414ca9c1f8189bde988ff26b168f5042cf
Vikas <vikreddevil@gmail.com>
2016-10-21 14:46:41 +0530

    color theme change

---
+++ app/src/main/java/com/snaptech/msb/WebService/ForcedUpdateData.java
@@ -0,0 +1,28 @@
+package com.snaptech.msb.WebService;
+
+/**
+ * Created by Aniket on 16-08-2016.
+ */
+public class ForcedUpdateData {
+
+    //{
+//        "code": 200,
+//        "message": "No Error",
+//        "data": {
+//        "id": 1,
+//        "android_version": 0,
+//        "ios_version": 0,
+//        "android_url": "https://play.google.com/store/apps/details?id=com.snaptech.emissio",
+//        "ios_url": null,
+//        "force_update_android": 0,
+//        "force_update_ios": 0
+//        }
+//        }
+    public int id;
+    public int android_version;
+    public int ios_version;
+    public String android_url;
+    public String ios_url;
+    public int force_update_android;
+    public int force_update_ios;
+}
