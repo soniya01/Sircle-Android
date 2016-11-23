f8c445499e0204bb521c1ece9bf0d999ae247471
Vikas <vikreddevil@gmail.com>
2016-11-23 16:22:08 +0530

    nahar color theme change

---
+++ app/src/main/java/com/snaptech/naharInt/DownLoader/FetchAppData.java
@@ -0,0 +1,145 @@
+package com.snaptech.naharInt.DownLoader;
+
+import android.os.AsyncTask;
+
+import com.snaptech.naharInt.Manager.DocumentManager;
+import com.snaptech.naharInt.Manager.EventManager;
+import com.snaptech.naharInt.Manager.LinksManager;
+import com.snaptech.naharInt.Manager.NotificationManager;
+import com.snaptech.naharInt.Manager.PhotoManager;
+import com.snaptech.naharInt.Manager.VideoManager;
+import com.snaptech.naharInt.UI.Model.Event;
+import com.snaptech.naharInt.UI.Model.Terms;
+import com.snaptech.naharInt.Utility.AppError;
+import com.snaptech.naharInt.Utility.Constants;
+import com.snaptech.naharInt.WebService.DocumentsResponse;
+import com.snaptech.naharInt.WebService.EventDataReponse;
+import com.snaptech.naharInt.WebService.LinksResponse;
+import com.snaptech.naharInt.WebService.NotificationResponse;
+import com.snaptech.naharInt.WebService.PhotoResponse;
+import com.snaptech.naharInt.WebService.VideoResponse;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by soniya on 10/10/15.
+ */
+public class FetchAppData extends AsyncTask<FetchAppData.FetchedDataDelegate, Void, Void> {
+
+private FetchedDataDelegate fetchedDataDelegate;
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected Void doInBackground(FetchedDataDelegate... params) {
+        fetchedDataDelegate = params[0];
+
+        PhotoManager.getSharedInstance().albumDetailsList.clear();
+        PhotoManager.getSharedInstance().albumsList.clear();
+        VideoManager.getSharedInstance().videoList.clear();
+        //NotificationManager.groupList.clear();
+        NotificationManager.notificationList.clear();
+        LinksManager.linksList.clear();
+        DocumentManager.docsList.clear();
+        DocumentManager.newsLetterList.clear();
+        EventManager.getSharedInstance().termsList.clear();
+        EventManager.getSharedInstance().eventList.clear();
+        EventManager.eventDetail = new Event();
+
+        String grpIdString = "";
+        for (int i = 0; i< NotificationManager.grpIds.size(); i++){
+            if (i == 0){
+                grpIdString = NotificationManager.grpIds.get(i);
+            }else {
+                grpIdString = grpIdString + "," + NotificationManager.grpIds.get(i) ;
+            }
+        }
+//        HashMap object = new HashMap();
+//        object.put("regId", Constants.GCM_REG_ID);
+//
+//        NotificationManager.getSharedInstance().getAllGroups(object, new NotificationManager.GroupsManagerListener() {
+//            @Override
+//            public void onCompletion(GroupResponse groupResponse, AppError error) {
+//
+//            }
+//        });
+
+        HashMap request = new HashMap();
+        request.put("regId", Constants.GCM_REG_ID);
+        request.put("groupId",grpIdString);
+        request.put("page", "1");
+
+        EventManager.getSharedInstance().getAllEvents(request, new EventManager.GetMonthwiseEventsManagerListener() {
+            @Override
+            public void onCompletion(EventDataReponse data, AppError error) {
+
+            }
+        });
+
+        EventManager.getSharedInstance().getAllTerms(null, new EventManager.GetAllTermsManagerListener() {
+            @Override
+            public void onCompletion(List<Terms> termsList, AppError error) {
+
+            }
+        });
+
+        DocumentManager.getSharedInstance().getAllDocs(request, new DocumentManager.GetDocumentManagerListener() {
+            @Override
+            public void onCompletion(DocumentsResponse response, AppError error) {
+
+            }
+        });
+
+        DocumentManager.getSharedInstance().getAllNewsLetters(request, new DocumentManager.GetDocumentManagerListener() {
+            @Override
+            public void onCompletion(DocumentsResponse response, AppError error) {
+
+            }
+        });
+
+        LinksManager.getSharedInstance().getAllLinks(request, new LinksManager.LinksManagerListener() {
+            @Override
+            public void onCompletion(LinksResponse response, AppError error) {
+
+            }
+        });
+
+        PhotoManager.getSharedInstance().getAlbums(request, new PhotoManager.GetAlbumsManagerListener() {
+            @Override
+            public void onCompletion(PhotoResponse response, AppError error) {
+
+            }
+        });
+
+        VideoManager.getSharedInstance().getAllVideos(request, new VideoManager.VideoManagerListener() {
+            @Override
+            public void onCompletion(VideoResponse response, AppError error) {
+
+            }
+        });
+
+
+        NotificationManager.getSharedInstance().getAllNotifications(request, new NotificationManager.NotificationManagerListener() {
+            @Override
+            public void onCompletion(NotificationResponse response, AppError error) {
+
+            }
+        });
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+        fetchedDataDelegate.fetchDataDone();
+    }
+
+    public static abstract class FetchedDataDelegate {
+        public abstract void fetchDataDone();
+    }
+}
---
+++ app/src/main/java/com/snaptech/naharInt/UI/Adapter/CamerarollAdapter.java
@@ -0,0 +1,69 @@
+package com.snaptech.naharInt.UI.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import com.snaptech.naharInt.DownLoader.ImageLoader;
+import com.snaptech.naharInt.R;
+import com.snaptech.naharInt.UI.Model.ImageData;
+
+import java.util.List;
+
+/**
+ * Created by soniya on 10/08/15.
+ */
+public class CamerarollAdapter extends BaseAdapter{
+
+    private Context context;
+    private List<ImageData> imageData;
+    private LayoutInflater layoutInflater;
+
+    public CamerarollAdapter(Context context, List<ImageData> imageData){
+        this.context = context;
+        this.imageData = imageData;
+        layoutInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    public List<ImageData> getImageData() {
+        return imageData;
+    }
+
+    public void setImageData(List<ImageData> imageData) {
+        this.imageData = imageData;
+    }
+
+    @Override
+    public int getCount() {
+        return imageData.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return imageData.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if(convertView == null){
+            layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.grid_row_camera_images, parent, false);
+        }
+
+        ImageView cameraImageView = (ImageView)convertView.findViewById(R.id.grid_image_view_picture);
+        ImageData data = (ImageData) getItem(position);
+//
+       ImageLoader.fetchImage(context, data, cameraImageView);
+
+        return convertView;
+    }
+
+}
