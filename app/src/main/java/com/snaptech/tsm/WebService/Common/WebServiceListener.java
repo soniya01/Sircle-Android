4687bd363656a612f35f954c37dfc76768114005
Vikas <vikreddevil@gmail.com>
2016-11-24 16:41:32 +0530

    color theme changes and bugs

---
+++ app/src/main/java/com/snaptech/tsm/WebService/Common/WebServiceListener.java
@@ -0,0 +1,11 @@
+package com.snaptech.tsm.WebService.Common;
+
+import com.snaptech.tsm.Utility.AppError;
+
+/**
+ * Created by soniya on 3/30/15.
+ */
+public interface WebServiceListener {
+
+    public void onCompletion(Object responseObject, AppError error);
+}
