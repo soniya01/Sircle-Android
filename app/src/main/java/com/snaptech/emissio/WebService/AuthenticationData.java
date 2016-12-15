05c0cd2dd89840e62307bb8f7db72d0c1652dbbd
Vikas <vikreddevil@gmail.com>
2016-12-15 19:06:58 +0530

    new zoom lib

---
+++ app/src/main/java/com/snaptech/emissio/WebService/AuthenticationData.java
@@ -0,0 +1,63 @@
+package com.snaptech.emissio.WebService;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by soniya on 02/09/15.
+ */
+public class AuthenticationData {
+
+    @SerializedName("access_token")
+    public String accessToken;
+
+    @SerializedName("expires_in")
+    public int expiresIn;
+
+    @SerializedName("token_type")
+    public String tokenType;
+
+    public String scope;
+
+    @SerializedName("refresh_token")
+    public String refreshToken;
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public int getExpiresIn() {
+        return expiresIn;
+    }
+
+    public void setExpiresIn(int expiresIn) {
+        this.expiresIn = expiresIn;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+}
