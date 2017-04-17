e58d6d24c24dec7ee89e687f2b79a064a95df01f
Vikas <vikreddevil@gmail.com>
2017-04-17 13:55:04 +0530

    msl new

---
+++ app/src/main/java/com/snaptech/mslgroupindia/WebService/Common/WebServiceListener.java
@@ -0,0 +1,11 @@
+package com.snaptech.mslgroupindia.WebService.Common;
+
+import com.snaptech.mslgroupindia.Utility.AppError;
+
+/**
+ * Created by soniya on 3/30/15.
+ */
+public interface WebServiceListener {
+
+    public void onCompletion(Object responseObject, AppError error);
+}
---
+++ app/src/main/java/com/snaptech/mslgroupindia/WebService/LinksResponse.java
@@ -0,0 +1,34 @@
+package com.snaptech.mslgroupindia.WebService;
+
+/**
+ * Created by soniya on 18/09/15.
+ */
+public class LinksResponse {
+    public int status;
+    public String message;
+    public LinksResponseData data ;
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LinksResponseData getData() {
+        return data;
+    }
+
+    public void setData(LinksResponseData data) {
+        this.data = data;
+    }
+}
